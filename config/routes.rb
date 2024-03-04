Alexander-Senko/organizer

Framework: generated a new Rails Engine
* Rails 7.1;
* Ruby 3.2.

config/routes.rb
@@ -1,0 +1,2 @@
+Organizer::Engine.routes.draw do
+end

lib/organizer/authors.rb
@@ -1,0 +1,15 @@
+Gem::Author ||= Struct.new(
+		:name,
+		:email,
+		:github_url,
+)
+
+module Organizer
+	AUTHORS = [
+			Gem::Author.new(
+					name:       'Alexander Senko',
+					email:      'Alexander.Senko@gmail.com',
+					github_url: 'https://github.com/Alexander-Senko',
+			),
+	]
+end

lib/organizer/engine.rb
@@ -1,0 +1,5 @@
+module Organizer
+  class Engine < ::Rails::Engine
+    isolate_namespace Organizer
+  end
+end

lib/organizer/version.rb
@@ -1,0 +1,3 @@
+module Organizer
+  VERSION = "0.1.0"
+end

test/dummy/config/routes.rb
@@ -1,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Organizer::Engine => "/organizer"
+end

Alexander-Senko/coniverse

config/routes.rb
@@ -1,0 +1,2 @@
+Coniverse::Engine.routes.draw do
+end

lib/coniverse/authors.rb
@@ -1,0 +1,15 @@
+Gem::Author ||= Struct.new(
+		:name,
+		:email,
+		:github_url,
+)
+
+module Coniverse
+	AUTHORS = [
+			Gem::Author.new(
+					name:       'Alexander Senko',
+					email:      'Alexander.Senko@gmail.com',
+					github_url: 'https://github.com/Alexander-Senko',
+			),
+	]
+end

lib/coniverse/engine.rb
@@ -1,0 +1,5 @@
+module Coniverse
+  class Engine < ::Rails::Engine
+    isolate_namespace Coniverse
+  end
+end

lib/coniverse/version.rb
@@ -1,0 +1,3 @@
+module Coniverse
+  VERSION = "0.1.0"
+end

test/dummy/config/routes.rb
@@ -1,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Coniverse::Engine => "/coniverse"
+end