ae111cd6c4caf5972b49602257fd26da40c53b66
Alexander Senko <Alexander.Senko@gmail.com>
2024-06-28 18:51:12 +0700

    Framework: use Action Text

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ with_options preload: true do
 	pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 end
 
+pin '@rails/activestorage', to: 'activestorage.esm.js'
+pin '@rails/actiontext',    to: 'actiontext.esm.js'
+pin 'trix'
+
 pin_all_from 'app/javascript/controllers', under: 'controllers'
