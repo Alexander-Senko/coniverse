5f6617b1e87b2ad635c6406980ae14061d771bfc
Alexander Senko <Alexander.Senko@gmail.com>
2024-11-01 01:34:39 +0700

    Framework: moved to Ruby 3.4

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
 engine_generators = Coniverse::Engine.config.generators
 
 Rails.application.config.generators do
-	_1.orm :active_record,
+	it.orm :active_record,
 			primary_key_type: engine_generators.options[engine_generators.orm][:primary_key_type]
 end
