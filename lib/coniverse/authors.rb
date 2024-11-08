12d5231f908a38226884100200526d3cbbfcd674
Alexander Senko <Alexander.Senko@gmail.com>
2024-11-08 23:27:21 +0700

    Framework / gem: improved `AUTHORS`
    ## Added
    
    * `#names`
    * `#emails`
    * `#github_url`

--- lib/coniverse/authors.rb
+++ lib/coniverse/authors.rb
@@ -1,15 +1,23 @@
 Gem::Author ||= Struct.new(
 		:name,
 		:email,
-		:github_url,
-)
+		:github,
+) do
+	def github_url = github && "https://github.com/#{github}"
+end
 
 module Coniverse
-	AUTHORS = [
+	AUTHORS = [ # rubocop:disable Style/MutableConstant
 			Gem::Author.new(
-					name:       'Alexander Senko',
-					email:      'Alexander.Senko@gmail.com',
-					github_url: 'https://github.com/Alexander-Senko',
+					name:   'Alexander Senko',
+					email:  'Alexander.Senko@gmail.com',
+					github: 'Alexander-Senko',
 			),
 	]
+
+	class << AUTHORS
+		def names      = filter_map &:name
+		def emails     = filter_map &:email
+		def github_url = filter_map(&:github_url).first
+	end
 end
