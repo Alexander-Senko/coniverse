18574fb13c24c380d1c872361a8225d8a1729d19
Alexander Senko <Alexander.Senko@gmail.com>
2024-11-27 07:15:32 +0700

    Views / messages: refactored
    Moved some logic to the presenter.
    
    * `MessagePresenter#heading`
    * `MessagePresenter#created`
    * `MessagePresenter#updated`

--- app/presenters/coniverse/message_presenter.rb
+++ app/presenters/coniverse/message_presenter.rb
@@ -1,5 +1,21 @@
 module Coniverse
 	class MessagePresenter < ApplicationPresenter
+		def heading
+			return unless title.present?
+
+			link
+					.tap { yield it if block_given? }
+		end
+
+		def created = created_at.tag class: 'created'
+		def updated = updated_at.tag class: 'updated'
+
+		def children(&)
+			turbo_frame_tag(:messages, src: messages_path, **{
+					class: 'messages',
+			}, &) if messages.any?
+		end
+
 		def flat?
 			return if messageless?
 
@@ -13,6 +29,12 @@ module Coniverse
 			return if messageless?
 
 			not flat?
+		end
+
+		def messages_path(**)
+			message_messages_path(self,
+					page: h.page&.settings,
+			**)
 		end
 	end
 end
