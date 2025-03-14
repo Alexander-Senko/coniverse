9c46a32baa4c678412f618cbbefba72a85107854
Alexander Senko <Alexander.Senko@gmail.com>
2025-03-14 20:34:55 +0700

    Views / layouts: refactored page titles
    * Moved the logic to `title` helper.
    * Added `Engine.title` (also available as `Coniverse.title`).
    * All the missing methods are delegated from `Coniverse` to
      `Coniverse::Engine`.

--- app/helpers/coniverse/application_helper.rb
+++ app/helpers/coniverse/application_helper.rb
@@ -1,5 +1,18 @@
 module Coniverse
 	module ApplicationHelper
+		def title
+			[
+					*(Array(@title)
+							.map(&:presence)
+					),
+
+					Engine.title,
+			]
+					.compact
+					.uniq
+					.join(' — ')
+		end
+
 		def render_if_exists path
 			render path if
 					lookup_context.exists? path, (path.include?(?/) ? [] : @lookup_context.prefixes), true
